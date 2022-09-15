e66f4030cf41ea18d566c5615c523318aaf12c98
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-09-15 15:51:56 +0100

    add contact inform

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import pic from './img/pic.jpg'
 import './App.css'
 
 
+const Contact = () => {
+  return (
+    <div>
+      <h1>Ismail Muhammed</h1>
+      <h3>IT Support Engr | DevOps Engr | Web Developer</h3>
+      <p>+234-703-237-8480</p>
+    </div>
+  )
+}
 
 const Image = () => {
   return (
-    <div>
+    <div className='pic'>
       <img src={pic} alt="pic" />
     </div>
     
