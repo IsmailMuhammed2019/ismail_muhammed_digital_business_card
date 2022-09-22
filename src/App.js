2e22427a7788e7e5325ceb04a7f9577fff02c53f
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-09-22 09:47:24 +0100

    add social component and about component

--- src/App.js
+++ src/App.js
@@ -1,90 +1,51 @@
 import React from 'react'
 import pic from './img/pic.jpg'
-import star from './img/star.png'
 import './App.css'
 
+const Social = () => {
+  return (
+    <div className='social'>
+      <button>Email</button>
+      <button>Linkedin</button>
+    </div>
+  )
+}
 
-// const Contact = () => {
-//   return (
-//     <div className='contact'>
-//       <div className=''>Email</div>
-
-//     </div>
-//   )
-// }
-
-// const Intro = () => {
-//   return (
-//     <div>
-//       <h1>Ismail Muhammed</h1>
-//       <h3>IT Support Engr | DevOps Engr | Web Developer</h3>
-//       <p>+234-703-237-8480</p>
-//     </div>
-//   )
-// }
-
-// const Image = () => {
-//   return (
-//     <div className='pic'>
-//       <img src={pic} alt="pic" />
-//     </div>
-    
-//   )
-// }
-
-
-// const InnerBox = () => {
-//   return (
-//     <div className='innerBox'>
-//       <Image />
-//       <Intro />
-//       {/* 
-//       <Contact />
-//       <SmallText />
-//       <Social /> */}
-//     </div>
-//   )
-// }
-
-
-
-// image holder
+const Intro = () => {
+  return (
+    <div>
+      <h1>Ismail Muhammed</h1>
+      <h3>IT Support Engr | DevOps Engr | Web Developer</h3>
+      <p>+234-703-237-8480</p>
+    </div>
+  )
+}
 
 const Image = () => {
   return (
-    <div className="propic">
-    <img src={pic} alt="my pic" />
+    <div className='pic'>
+      <img src={pic} alt="pic" />
     </div>
+    
   )
 }
-// info
 
-const Info = () => {
+
+const InnerBox = () => {
   return (
-    <div className='start'>
-      <img src={star} alt="star image" />
-      <h2>Ismail Muhammed</h2>
+    <div className='innerBox'>
+      <Image />
+      <Intro />
+      <Social />
+
     </div>
   )
 }
-
-//contacts
-const Contacts = () => {
-  return (
-    <div className="contacts">
-      <h3>+234 703 237 8480</h3>
-      <h3>ismailmuhammed2019@gmail.com</h3>
-    </div>
-  )
-}
-
 
 function App() {
   return (
     <div className='outerRectangle'>
-      <Image />
-      <Info />
-      <Contacts />
+      <InnerBox />
     </div>
   )
 }
